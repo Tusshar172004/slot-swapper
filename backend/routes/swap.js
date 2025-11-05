eca707f65b97f84d2ee59319aae9e04940c408a6
Tushar Patil <155620600+Tusshar172004@users.noreply.github.com>
2025-11-05 22:19:43 +0530

    Update swap.js

--- backend/routes/swap.js
+++ backend/routes/swap.js
@@ -1,4 +1,3 @@
-// backend/routes/swap.js
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
@@ -6,13 +5,10 @@ const swapController = require('../controllers/swapController');
 
 router.use(auth);
 
-// create swap request
 router.post('/request', swapController.createSwapRequest);
 
-// respond to a swap (accept/reject)
 router.post('/response/:requestId', swapController.respondToSwap);
 
-// list incoming/outgoing
 router.get('/incoming', swapController.getIncoming);
 router.get('/outgoing', swapController.getOutgoing);
 
